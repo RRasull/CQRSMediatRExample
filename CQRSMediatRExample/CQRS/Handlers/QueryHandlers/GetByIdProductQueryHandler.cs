fd101088a47585d07efe26d6018914c2a86e2ebd
RRasull <92165344+RRasull@users.noreply.github.com>
2022-05-11 14:32:10 +0400

    'refactor: modify GetByIdProductQueryHandler and return response'

--- CQRSMediatRExample/CQRS/Handlers/QueryHandlers/GetByIdProductQueryHandler.cs
+++ CQRSMediatRExample/CQRS/Handlers/QueryHandlers/GetByIdProductQueryHandler.cs
@@ -1,6 +1,8 @@
 ﻿using CQRSMediatRExample.CQRS.Queries.Request;
 using CQRSMediatRExample.CQRS.Queries.Response;
+using CQRSMediatRExample.DAL;
 using MediatR;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,9 +10,27 @@ namespace CQRSMediatRExample.CQRS.Handlers.QueryHandlers
 {
     public class GetByIdProductQueryHandler : IRequestHandler<GetByIdProductQueryRequest, GetByIdProductQueryResponse>
     {
+
+        private readonly AppDbContext _context;
+
+
+        public GetByIdProductQueryHandler(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
         public Task<GetByIdProductQueryResponse> Handle(GetByIdProductQueryRequest request, CancellationToken cancellationToken)
         {
-            throw new System.NotImplementedException();
+            var product = _context.Products.Where(x => x.Id == request.ProductId.Id).FirstOrDefault();
+            var response = new GetByIdProductQueryResponse
+            {
+                Id = product.Id,
+                Name = product.Name,
+                Price = product.Price
+            };
+
+            return Task.FromResult(response);
         }
     }
 }
--- CQRSMediatRExample/CQRS/Queries/Request/GetByIdProductQueryRequest.cs
+++ CQRSMediatRExample/CQRS/Queries/Request/GetByIdProductQueryRequest.cs
@@ -1,10 +1,11 @@
 ﻿using CQRSMediatRExample.CQRS.Queries.Response;
+using CQRSMediatRExample.Entities;
 using MediatR;
 
 namespace CQRSMediatRExample.CQRS.Queries.Request
 {
     public class GetByIdProductQueryRequest : IRequest<GetByIdProductQueryResponse>
     {
-        public int Id { get; set; }
+        public Product ProductId { get; set; }
     }
 }
