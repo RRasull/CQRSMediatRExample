20caa7f322f1ab4d2746803320a2baedbfb98433
RRasull <92165344+RRasull@users.noreply.github.com>
2022-05-11 14:49:48 +0400

    'feat: add Get method for Product'

--- CQRSMediatRExample/Controllers/ProductController.cs
+++ CQRSMediatRExample/Controllers/ProductController.cs
@@ -10,6 +10,18 @@ namespace CQRSMediatRExample.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
-       
+        private readonly IMediator _mediatR;
+
+        public ProductController(IMediator mediatR)
+        {
+            _mediatR = mediatR;
+        }
+
+        [HttpGet("id")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var query = new GetByIdProductQueryRequest() { Id = id };
+            return Ok(await _mediatR.Send(query));
+        }
     }
 }
