e8b8f0320f9dca3a1e69b6ef39ca7c9a51df6f8e
RRasull <92165344+RRasull@users.noreply.github.com>
2022-05-11 13:12:05 +0400

    'feat: add Get Method for all Products'

---
+++ CQRSMediatRExample/CQRS/Queries/Response/GetAllProductQueryResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace CQRSMediatRExample.CQRS.Queries.Response
+{
+    public class GetAllProductQueryResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
